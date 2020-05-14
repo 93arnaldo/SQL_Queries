19f202a5fdcc8ecf36176e3b005161099e9a3441
Arnaldo Vera <45240043+93arnaldo@users.noreply.github.com>
2020-05-14 10:40:31 -0700

    Update 2_material_purchased_more_times_Oct2019.sql

--- queries/2_material_purchased_more_times_Oct2019.sql
+++ queries/2_material_purchased_more_times_Oct2019.sql
@@ -1,13 +1,13 @@
-/*WHICH BUILDING MATERIAL HAS BEEN PURCHASED THE MOST TIMES IN OCTOBER 2019.*/
+/*WHICH MATERIAL HAS BEEN PURCHASED THE MOST TIMES IN OCTOBER 2019?*/
 
 SELECT
   m.name
 FROM
-  building_material m
+  materials m
 INNER JOIN (--Subquery to select the materials with the most purchases
   SELECT
     MAX(quantity),
-    building_material_id,
+    material_id,
     -- Month and Year belong to the subquery because that way, we don't need to join the two tables outside of the subquery (materials,purchases)
     EXTRACT (MONTH FROM datetime) AS Month,
     EXTRACT (YEAR FROM datetime) AS Year
@@ -17,4 +17,4 @@ INNER JOIN (--Subquery to select the materials with the most purchases
     Month = 10
     AND Year = 2019) max_quant
 ON
-  max_quant.building_material_id = m.id;
+  max_quant.material_id = m.id;
