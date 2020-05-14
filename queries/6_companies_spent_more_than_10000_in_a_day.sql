dd56db1d0431620f2eed09f89ba0c38dada2d0d5
Arnaldo Vera <45240043+93arnaldo@users.noreply.github.com>
2020-05-14 10:50:01 -0700

    Update 6_companies_spent_more_than_10000_in_a_day.sql

--- queries/6_companies_spent_more_than_10000_in_a_day.sql
+++ queries/6_companies_spent_more_than_10000_in_a_day.sql
@@ -1,18 +1,18 @@
-/*LIST OF CONSTRUCTION COMPANIES THAT HAS SPENT MORE THAN $10,000 IN A SINGLE DAY*/
+/*LIST OF CONSTRUCTION COMPANIES THAT HAVE SPENT MORE THAN $10,000 IN A SINGLE DAY*/
 SELECT
   distinct c.name, -- Including distinct name because we could have that a company spent more than 10000  more than once in a day. It also depends on what we want to know, perhaps we want to know how many times.
   (p.quantity*b.price) AS trans_cost,
   date_trunc ('day', p.datetime) AS day
 FROM
-  building_material b
+  materials b
 RIGHT OUTER JOIN
-  purchase p
+  purchases p
 ON
-  p.building_material_id = b.id
+  p.material_id = b.id
 LEFT OUTER JOIN
-  construction_company c
+  companies c
 ON
-  p.construction_company_id = c.id
+  p.company_id = c.id
 HAVING
   trans_cost > 1000000
 GROUP BY
