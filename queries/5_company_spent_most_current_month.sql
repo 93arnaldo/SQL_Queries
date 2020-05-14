b7aa7b3e27f921ebf5fa754f0dc9fd2462bebdae
Arnaldo Vera <45240043+93arnaldo@users.noreply.github.com>
2020-05-14 10:47:10 -0700

    Update 5_company_spent_most_current_month.sql

--- queries/5_company_spent_most_current_month.sql
+++ queries/5_company_spent_most_current_month.sql
@@ -1,20 +1,20 @@
-/*WHICH CONSTRUCTION COMPANY SPENT THE MOST MONEY IN THE CURRENT MONTH*/
+/*WHICH COMPANY SPENT THE MOST MONEY IN THE CURRENT MONTH?*/
 
 SELECT
   c.name,
 FROM
-  construction_company c
+  companies c
 INNER JOIN ( --Subquery to select the company that spent the most money in each month
   SELECT 
     max(p.quantity*b.price) AS max_trans_cost, -- max function to get the highest transation cost.
-    p.contruction_company_id AS company_id,
+    p.company_id AS company_id,
     convert(month, getDATE()) AS current_month
   FROM
     purchase p
   INNER JOIN
-    building_material b
+    materials b
   ON
-    p.building_material_id = b.id
+    p.material_id = b.id
   WHERE
     convert(month, p.datetime) = current_month) last_month_trans_cost_t
 ON
